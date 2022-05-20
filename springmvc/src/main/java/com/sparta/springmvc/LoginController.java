bd54627ee6588bfc7839e1e4ca7595830da9ff0a
ParkSungGyu1 <steven0507@naver.com>
2022-05-20 15:30:38 +0900

    API 구현 연습

--- springmvc/src/main/java/com/sparta/springmvc/LoginController.java
+++ springmvc/src/main/java/com/sparta/springmvc/LoginController.java
@@ -1,5 +1,36 @@
 package com.sparta.springmvc;
 
-public class LoginController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String loginGet(){
+        return "redirect:/login-form.html";
+    }
+
+    @PostMapping("/login")
+    public String loginPost(@ModelAttribute UserDto userDto,Model model){
+        if(userDto.getId().equals(userDto.getPassword())){
+            model.addAttribute("loginId" , userDto.getId());
+
+        }
+        return "login-result";
+    }
+
+    /*@PostMapping("/login")
+    public String loginProcess(
+            @RequestParam String id,
+            @RequestParam String password,
+            Model model
+    ) {
+        if (id.equals(password)) {
+            model.addAttribute("loginId", id);
+        }
+
+        return "login-result";
+    }*/
 }
---
+++ springmvc/src/main/java/com/sparta/springmvc/UserDto.java
@@ -0,0 +1,13 @@
+package com.sparta.springmvc;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class UserDto {
+    private String id;
+    private String password;
+}
