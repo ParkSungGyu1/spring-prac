dac4e8dcf955e27c05d4ecd802d2b026c591fb43
ParkSungGyu1 <steven0507@naver.com>
2022-05-23 18:56:30 +0900

    페이징 추가

--- springcore/src/main/java/com/sparta/springcore/service/ProductService.java
+++ springcore/src/main/java/com/sparta/springcore/service/ProductService.java
@@ -5,8 +5,12 @@ import com.sparta.springcore.dto.ProductRequestDto;
 import com.sparta.springcore.model.Product;
 import com.sparta.springcore.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
@@ -40,12 +44,19 @@ public class ProductService {
     }
 
     // 회원 ID 로 등록된 상품 조회
-    public List<Product> getProducts(Long userId) {
-        return productRepository.findAllByUserId(userId);
+    public Page<Product> getProducts(Long userId, int page, int size, String sortBy, boolean isAsc) {
+
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = (Pageable) PageRequest.of(page,size,sort);
+        return productRepository.findAllByUserId(userId, pageable);
     }
 
     // 모든 상품 조회 (관리자용)
-    public List<Product> getAllProducts() {
+    public List<Product> getAllProducts(int page, int size, String sortBy, boolean isAsc) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = (Pageable) PageRequest.of(page,size,sort);
         return productRepository.findAll();
     }
 }
