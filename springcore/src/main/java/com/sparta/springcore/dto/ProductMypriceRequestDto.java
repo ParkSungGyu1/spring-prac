fbb0f23c42fd99d1bf3cce543e2b6357204fe4a3
ParkSungGyu1 <steven0507@naver.com>
2022-05-23 17:33:56 +0900

    admin 처리

--- springcore/src/main/java/com/sparta/springcore/dto/ProductMypriceRequestDto.java
+++ springcore/src/main/java/com/sparta/springcore/dto/ProductMypriceRequestDto.java
@@ -3,9 +3,11 @@ package com.sparta.springcore.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
 @Getter
 public class ProductMypriceRequestDto {
     private int myprice;
--- springcore/src/main/java/com/sparta/springcore/service/ProductService.java
+++ springcore/src/main/java/com/sparta/springcore/service/ProductService.java
@@ -1,47 +1,51 @@
 package com.sparta.springcore.service;
 
-import com.sparta.springcore.model.Product;
 import com.sparta.springcore.dto.ProductMypriceRequestDto;
-import com.sparta.springcore.repository.ProductRepository;
 import com.sparta.springcore.dto.ProductRequestDto;
+import com.sparta.springcore.model.Product;
+import com.sparta.springcore.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
+
     private final ProductRepository productRepository;
 
-    public ProductService(ProductRepository productRepository){
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
-    public Product createProduct(ProductRequestDto requestDto){
-        // 요청받은 DTO 로 DB에 저장할 객체 만들기
-        Product product = new Product(requestDto);
+    public Product createProduct(ProductRequestDto requestDto, Long userId ) {
+// 요청받은 DTO 로 DB에 저장할 객체 만들기
+        Product product = new Product(requestDto, userId);
 
-        this.productRepository.save(product);
+        productRepository.save(product);
 
         return product;
     }
 
-    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto){
-        Product product = this.productRepository.findById(id)
+    public Product updateProduct(Long id, ProductMypriceRequestDto requestDto) {
+        Product product = productRepository.findById(id)
                 .orElseThrow(() -> new NullPointerException("해당 아이디가 존재하지 않습니다."));
 
         int myprice = requestDto.getMyprice();
         product.setMyprice(myprice);
-        this.productRepository.save(product);
+        productRepository.save(product);
 
         return product;
-
     }
 
-    public List<Product> getProducts(){
-        List<Product> product = this.productRepository.findAll();
+    // 회원 ID 로 등록된 상품 조회
+    public List<Product> getProducts(Long userId) {
+        return productRepository.findAllByUserId(userId);
+    }
 
-        return product;
-
-
+    // 모든 상품 조회 (관리자용)
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
